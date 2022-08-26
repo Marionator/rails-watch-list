ee1218dc0dda2b0597c6c348e4ca312d25a85175
Marion Salaun <salaun.marion@gmail.com>
2022-08-26 17:08:23 +0100

    whatever

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,10 +1,6 @@
 class List < ApplicationRecord
-  # When you delete a list, you should delete all
-  # associated bookmarks (but not the movies as they
-  # can be referenced in other lists) => seems to be
-  # in the difference btwn :destroy & :delete, with
-  # different behaviours in AR
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+
   validates :name, presence: true, uniqueness: true
 end
